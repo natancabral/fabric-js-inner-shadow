c1a9925803f1779304c13f24681fa58073c90e3f
Natan Cabral <natancabral@gnial.com.br>
2021-04-02 16:58:11 -0300

    Add files via upload

--- src/inner-shadow.js
+++ src/inner-shadow.js
@@ -85,7 +85,7 @@
     const clipOfObject = clone( object );
     rect.clipPath = clipOfObject;
     rect.clipPath.set({
-      inverted: true, // this magic
+      inverted: true, 
       left: ( rect.clipPath.width / 2 ) * -1,
       top: ( rect.clipPath.height / 2 ) * -1,
     });
@@ -124,9 +124,99 @@
 
   }
 
+  // https://jsfiddle.net/jrekw5og/141/
+  // https://stackoverflow.com/questions/37378088/inset-shadow-on-html5-canvas-image/37380488#37380488
+  function createObjectInnerShadow2(){
+   
+    const object = canvas.getActiveObject();
+
+    // set first object
+    if( object === undefined || object === null ){
+      window.alert('add and select a object');
+      return;
+    }
+
+    var data = object.toDataURL({
+      enableRetinaScaling: true,
+      format: "png", // png | jpg
+      // quality: 1, // only jpg
+      multiplier: 1, // scale, default 1
+    });
+
+    var img = document.createElement("img");
+    img.onload = function(){ createANewCanva(this) };
+    img.src = data;
+
+  }
+
+  function createANewCanva (img) {
+
+      var distance = 10;
+      var alpha = .5
+
+      //the size of the shadow depends on the size of the target,
+      //then I will create extra "walls" around the picture to be sure
+      //tbat the shadow will be correctly filled (with the same intensity everywhere)
+      //(it's not obvious with this image, but it is when there is no space at all between the image and its border)
+      var offset = 50 + distance;
+      var hole = document.createElement("canvas");
+      var holeContext = hole.getContext("2d");
+      hole.width = img.width + offset*2;
+      hole.height = img.height + offset*2;
+
+      //first, I draw a big black rect
+      holeContext.fillStyle = "#000000";
+      holeContext.fillRect(0,0,hole.width,hole.height);
+      
+      //then I use the image to make an hole in it
+      holeContext.globalCompositeOperation = "destination-out";
+      holeContext.drawImage(img,offset,offset);
+      
+      
+      //I create a new canvas that will contains the shadow of the hole only
+      var shadow = document.createElement("canvas");
+      var shadowContext = shadow.getContext("2d");
+      shadow.width = img.width;
+      shadow.height = img.height;
+      shadowContext.filter = "drop-shadow(0px 0px "+distance+"px #000000 ) ";
+      shadowContext.drawImage(hole,-offset,-offset);
+      shadowContext.globalCompositeOperation = "destination-out";
+      shadowContext.drawImage(hole,-offset,-offset);
+      
+      //now, because the default-shadow filter is really to soft, I normalize the shadow 
+      //then I will be sure that the alpha-gradient of the shadow will start at "shadowAlpha" and end at 0
+      normalizeAlphaShadow(shadow,alpha);
+      
+      //Finally, I create another canvas that will contain the image and the shadow over it
+      var result = document.createElement("canvas");
+      result.width = img.width;
+      result.height = img.height;
+      var context = result.getContext("2d");
+      context.drawImage(img,0,0)
+      context.drawImage(shadow,0,0);
+      
+      //and that's it !
+      document.body.appendChild(result);
+
+ }
+ 
+ function normalizeAlphaShadow(c,alpha){
+    var imageData = c.getContext("2d").getImageData(0,0,c.width,c.height);
+    var pixelData = imageData.data;
+    var i,len = pixelData.length;
+    var max = 0;
+    for(i=3;i<len;i+=4) if(pixelData[i]>max) max = pixelData[i];
+    
+    max = (255/max) * alpha;
+    for(i=3;i<len;i+=4) pixelData[i] *= max;
+    
+    c.getContext("2d").putImageData(imageData,0,0)
+ }
+
   document.getElementById('addObjectRect').addEventListener("click", addObjectRect);
   document.getElementById('addObjectCircle').addEventListener("click", addObjectCircle);
   document.getElementById('addObjectPath').addEventListener("click", addObjectPath);
   document.getElementById('createObjectInnerShadow').addEventListener("click", createObjectInnerShadow);
+  document.getElementById('createObjectInnerShadow2').addEventListener("click", createObjectInnerShadow2);
 
 })()
