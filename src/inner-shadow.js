deee8a2cfb5dc71c14c9ef95aff7c35c3e54c275
Natan Cabral <natancabral@gnial.com.br>
2021-04-03 12:57:09 -0300

    Add files via upload

--- src/inner-shadow.js
+++ src/inner-shadow.js
@@ -3,6 +3,20 @@
   const canvas = new fabric.Canvas('innershadow',{
     preserveObjectStacking: true
   });
+  canvas.on('after:render', () => {
+    // cria um retangulo de area do objeto
+    canvas.contextContainer.strokeStyle = '#444444';  
+    canvas.forEachObject( ( obj ) => {
+      const { left, top, width, height } = obj.getBoundingRect();
+      canvas.contextContainer.strokeRect(
+        left + 0.5,
+        top + 0.5,
+        width,
+        height
+      );
+    })
+  });
+
 
   // clone efficient function
   const clone = (object) => {
@@ -50,8 +64,8 @@
 
   const createObjectInnerShadow = () => {
 
-    const object = canvas.getActiveObject();
-    const offset = 10;
+    let offset = 10;
+    let object = canvas.getActiveObject();
 
     // set first object
     if( object === undefined || object === null ){
@@ -59,9 +73,49 @@
       return;
     }
 
+    // functions
+    // -----------------------------------------------------------------------------
+    const setAngle = ( c, elements, angle, multiplier ) => {
+      const group = new fabric.Group(elements);
+      group.rotate( angle === undefined ? 0 : angle * multiplier ); // 1|-1
+      group.destroy()
+      c.renderAll();
+    }
+    
+    const getTransform = ( c, o ) => {
+      // calculate the total transformation that is applied to the objects pixels:
+      const mCanvas   = c.viewportTransform;
+      const mObject   = o.calcTransformMatrix();
+      const mTotal    = fabric.util.multiplyTransformMatrices(mCanvas, mObject); // inverting the order gives wrong result
+      const options   = fabric.util.qrDecompose(mTotal);
+      const values    = {...options, flipX: o.flipX, flipY: o.flipY };
+      //delete values.angle;
+      // reset values
+      o.set({ scaleX: 1, scaleY: 1, flipX: false, flipY: false, });
+      // return transform values
+      return values;
+    }
+
+    const setTransform = ( c, o, values ) => {
+      const group = new fabric.Group( o );
+      group.set( values );
+      group.destroy();
+      c.renderAll();
+    }
+    // -----------------------------------------------------------------------------
+
+    const { angle } = object;
+
+    // -----------------------------------------------------------------------------
+    // set angle 0
+    setAngle( canvas, [object], angle, -1 );
+    // get transform and reset values
+    const valuesTransform = getTransform( canvas, object );    
+    // -----------------------------------------------------------------------------
+
     // phase 1
     // get values of object , dimensions and position
-    const { width, height, left, top, angle } = object;
+    const { width, height, left, top } = object;
     // create a rect object
     const rect = new fabric.Rect({
       typeName: 'Rect',
@@ -69,6 +123,8 @@
       height: height + ( offset * 2 ),  // set margin/offset
       left: left - offset,
       top: top - offset,
+      // scaleX, scaleX,
+      // scaleY, scaleY,
       fill: "black",
       // set shadow
       shadow: {
@@ -78,7 +134,6 @@
         offsetY: 0,
       },
     });
-    rect.setCoords();
 
     // phase 2 - apply inverted mask 
     // create a clone of object and transform like a mask inverted
@@ -102,30 +157,22 @@
     // add all on canvas
     canvas.add(clipFinal);
     canvas.bringToFront(clipFinal);
-    canvas.renderAll();
-    
-    // shadow: {
-    //   color: "rgba(100,100,100,.9)",
-    //   blur: 15,
-    //   offsetX: 0,
-    //   offsetY: 0,
-    // },
 
-    // canvas.getActiveObject().on('event:moved', function(event){
-    //   const mySelf = event.target;
-    //   console.log('>>',mySelf.left,mySelf.get('left'));
-    //   clipFinal.add({
-    //     left: mySelf.left,
-    //     top: mySelf.top,
-    //     width: mySelf.width,
-    //     height: mySelf.height,
-    //   });
-    // })
+    // -----------------------------------------------------------------------------
+    // set old transform
+    setTransform( canvas, [object,clipFinal], valuesTransform );
+    // set old angle
+    setAngle( canvas, [object,clipFinal], angle, 1 );
+    // -----------------------------------------------------------------------------
+
+    // render
+    canvas.renderAll();
 
   }
 
   // https://jsfiddle.net/jrekw5og/141/
   // https://stackoverflow.com/questions/37378088/inset-shadow-on-html5-canvas-image/37380488#37380488
+
   function createObjectInnerShadow2(){
    
     const object = canvas.getActiveObject();
@@ -154,26 +201,25 @@
       var distance = 10;
       var alpha = .5
 
-      //the size of the shadow depends on the size of the target,
-      //then I will create extra "walls" around the picture to be sure
-      //tbat the shadow will be correctly filled (with the same intensity everywhere)
-      //(it's not obvious with this image, but it is when there is no space at all between the image and its border)
+      // the size of the shadow depends on the size of the target,
+      // then I will create extra "walls" around the picture to be sure
+      // tbat the shadow will be correctly filled (with the same intensity everywhere)
+      // (it's not obvious with this image, but it is when there is no space at all between the image and its border)
       var offset = 50 + distance;
       var hole = document.createElement("canvas");
       var holeContext = hole.getContext("2d");
       hole.width = img.width + offset*2;
       hole.height = img.height + offset*2;
 
-      //first, I draw a big black rect
+      // first, I draw a big black rect
       holeContext.fillStyle = "#000000";
       holeContext.fillRect(0,0,hole.width,hole.height);
       
-      //then I use the image to make an hole in it
+      // then I use the image to make an hole in it
       holeContext.globalCompositeOperation = "destination-out";
       holeContext.drawImage(img,offset,offset);
       
-      
-      //I create a new canvas that will contains the shadow of the hole only
+      // I create a new canvas that will contains the shadow of the hole only
       var shadow = document.createElement("canvas");
       var shadowContext = shadow.getContext("2d");
       shadow.width = img.width;
@@ -183,11 +229,11 @@
       shadowContext.globalCompositeOperation = "destination-out";
       shadowContext.drawImage(hole,-offset,-offset);
       
-      //now, because the default-shadow filter is really to soft, I normalize the shadow 
-      //then I will be sure that the alpha-gradient of the shadow will start at "shadowAlpha" and end at 0
+      // now, because the default-shadow filter is really to soft, I normalize the shadow 
+      // then I will be sure that the alpha-gradient of the shadow will start at "shadowAlpha" and end at 0
       normalizeAlphaShadow(shadow,alpha);
       
-      //Finally, I create another canvas that will contain the image and the shadow over it
+      // Finally, I create another canvas that will contain the image and the shadow over it
       var result = document.createElement("canvas");
       result.width = img.width;
       result.height = img.height;
@@ -195,28 +241,24 @@
       context.drawImage(img,0,0)
       context.drawImage(shadow,0,0);
       
-      //and that's it !
+      // and that's it !
       document.body.appendChild(result);
 
-
-      // send to fabric js
       // ----------------------------------------------------------------------------------------
-      // ----------------------------------------------------------------------------------------
-
-      //var dataBase = result.toDataURL("image/png");
+      // now send to fabric js
+      const object = canvas.getActiveObject();
+      // var dataBase = result.toDataURL("image/png");
       var imgInstance = new fabric.Image(result, {
-      left: 100,
-      top: 100,
+        left: object.getBoundingRect().left,
+        top: object.getBoundingRect().top,
       });
       canvas.add(imgInstance);
       // ----------------------------------------------------------------------------------------
-      // ----------------------------------------------------------------------------------------
-      
-
 
  }
  
- function normalizeAlphaShadow(c,alpha){
+  function normalizeAlphaShadow(c,alpha){
+
     var imageData = c.getContext("2d").getImageData(0,0,c.width,c.height);
     var pixelData = imageData.data;
     var i,len = pixelData.length;
@@ -227,12 +269,57 @@
     for(i=3;i<len;i+=4) pixelData[i] *= max;
     
     c.getContext("2d").putImageData(imageData,0,0)
- }
+  
+  }
+
+  function createObjectInnerShadow3(){
+
+    const object = canvas.getActiveObject();
+
+    // set first object
+    if( object === undefined || object === null ){
+      window.alert('add and select a object');
+      return;
+    }
+
+    var data = object.toDataURL({
+      enableRetinaScaling: true,
+      format: "png", // png | jpg
+      // quality: 1, // only jpg
+      multiplier: 1, // scale, default 1
+    });
+
+
+    var ctx = canvas.getContext("2d"), img = new Image;
+    img.onload = function() {
+
+      // draw in image to main canvas
+      ctx.drawImage(this, 0, 0);
+
+      // invert alpha channel
+      ctx.globalCompositeOperation = "xor";
+      ctx.fillRect(0, 0, c.width, c.height);
+
+      // draw itself again using drop-shadow filter
+      ctx.shadowBlur = 7*2;  // use double of what is in CSS filter (Chrome x4)
+      ctx.shadowOffsetX = ctx.shadowOffsetY = 5;
+      ctx.shadowColor = "#000";
+      ctx.drawImage(c, 0, 0);
+
+      // draw original image with background mixed on top
+      ctx.globalCompositeOperation = "destination-atop";
+      ctx.shadowColor = "transparent";                  // remove shadow !
+      ctx.drawImage(this, 0, 0);
+    }
+    img.src = data;
+
+  }
 
   document.getElementById('addObjectRect').addEventListener("click", addObjectRect);
   document.getElementById('addObjectCircle').addEventListener("click", addObjectCircle);
   document.getElementById('addObjectPath').addEventListener("click", addObjectPath);
   document.getElementById('createObjectInnerShadow').addEventListener("click", createObjectInnerShadow);
   document.getElementById('createObjectInnerShadow2').addEventListener("click", createObjectInnerShadow2);
+  document.getElementById('createObjectInnerShadow3').addEventListener("click", createObjectInnerShadow3);
 
 })()
